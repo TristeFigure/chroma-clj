ce9ca3bbae028d2b3eda1a8b5ff680c09d239d21
TristeFigure <funtain@me.com>
2023-06-08 00:55:14 +0200

    Fill in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject chroma-clj "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "Clojure wrapper for ChromaDB"
+  :url "https://github.com/TristeFigure/chroma-clj"
+  :license {:name "Apache 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [clj-python/libpython-clj "2.024"]]
   :repl-options {:init-ns chroma-clj.core}
